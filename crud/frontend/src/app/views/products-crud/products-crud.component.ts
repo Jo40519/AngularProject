87c04e34561a0c8bb02c4c67d86e87bf547c32f0
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2021-12-09 08:00:18 -0300

    Adicionando as rotas
    Colocando as rotas de homeComponent e ProductsCompnente.

--- crud/frontend/src/app/views/products-crud/products-crud.component.ts
+++ crud/frontend/src/app/views/products-crud/products-crud.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-products-crud',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsCrudComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(
+  ) { }
 
   ngOnInit(): void {
   }
+
 
 }
