ee922885d9a93c9d76fb1c585f0dd738ccd4a396
João Vitor Nunes Lobato de Souza <jaolobato85@gmail.com>
2021-12-10 08:23:21 -0300

    Diretivas estruturais
    Agora com Diretivas Estruturais, onde pode definir uma sintaxe para que possa mexer em estruturar do DOM.

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -13,7 +13,8 @@ import {MatListModule} from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import {MatCardModule} from '@angular/material/card';
 import { ProductsCrudComponent } from './views/products-crud/products-crud.component';
-import { RedDirective } from './directives/red.directive'
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive'
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import { RedDirective } from './directives/red.directive'
     NavComponent,
     HomeComponent,
     ProductsCrudComponent,
-    RedDirective
+    RedDirective,
+    ForDirective
   ],
   imports: [
     BrowserModule,
